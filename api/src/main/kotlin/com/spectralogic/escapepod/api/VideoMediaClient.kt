dce97e1ca63f7c9661170778a9db3712d17f41a7
GraciesPadre <ShastasPadre1@gmail.com>
2017-08-15 16:40:42 -0600

    Abstracting the migration job id.

--- api/src/main/kotlin/com/spectralogic/escapepod/api/VideoMediaClient.kt
+++ api/src/main/kotlin/com/spectralogic/escapepod/api/VideoMediaClient.kt
@@ -1,3 +1,18 @@
+/*
+ * ****************************************************************************
+ *    Copyright 2014-2017 Spectra Logic Corporation. All Rights Reserved.
+ *    Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *    this file except in compliance with the License. A copy of the License is located at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    or in the "license" file accompanying this file.
+ *    This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *    CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *    specific language governing permissions and limitations under the License.
+ *  ****************************************************************************
+ */
+
 package com.spectralogic.escapepod.api
 
 import io.reactivex.Observable
@@ -11,10 +26,10 @@ interface VideoMediaClient {
     // Returns migration job id
     // Category? -- FlashNet doesn't have that concept as a separate entity
     // Asynchronous: Call to start a migration.  Implement observable to know when migration is complete
-    fun beginMigration(itemsToMigrate : Sequence<StorageGroupItem>, migrationDestination : String, priority : MigrationPriority) : Observable<Long>
+    fun <T> beginMigration(itemsToMigrate : Sequence<StorageGroupItem>, migrationDestination : String, priority : MigrationPriority) : Observable<MigrationJobId<T>>
 
     // TODO: Create abstraction for migrationJobId
-    fun getMigrationStatus(migrationJobId : Long) : Observable<MigrationStatus>
+    fun <T> getMigrationStatus(migrationJobId : MigrationJobId<T>) : Observable<MigrationStatus>
 
-    fun cancelMigration(migrationJobId : Long) : Observable<String>
+    fun <T> cancelMigration(migrationJobId : MigrationJobId<T>) : Observable<String>
 }
