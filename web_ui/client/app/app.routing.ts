df6e3e8ac66bc8a90a22bd30097ce5fd5ec4a9b1
GraciesPadre <ShastasPadre1@gmail.com>
2017-09-22 10:39:41 -0600

    Fixing some tslint whines. Updating routing info to work with browsers that don't support html 5.

--- web_ui/client/app/app.routing.ts
+++ web_ui/client/app/app.routing.ts
@@ -3,7 +3,7 @@ import { RouterModule } from "@angular/router";
 import { Routes } from "@angular/router";
 
 const routes: Routes = [
-    { path: 'search', loadChildren: 'app/search/search.module'}
+    { path: 'search', loadChildren: 'app/search/search.module#SearchModule'}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
